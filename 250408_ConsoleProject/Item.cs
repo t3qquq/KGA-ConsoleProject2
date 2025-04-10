a8f78147d880d7c07d7edeca84a56cc95b41fc2e
t3qquq <63445961+t3qquq@users.noreply.github.com>
2025-04-10 11:17:03 +0900

    [feat] Item, HealingPostion 추가

---
+++ 250408_ConsoleProject/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _250408_ConsoleProject
+{
+	public abstract class Item
+	{
+		public abstract string Name { get; }
+		public abstract void Use(Player player);
+	}
+
+	public class HealingPotion : Item
+	{
+		public override string Name => "회복 포션";
+		public override void Use(Player player)
+		{
+			player.HP += 50;
+		}
+	}
+
+}
